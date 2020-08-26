5fe9f28eb55bfbb7d4ba32424464a2106b0797ab
mykhailo-vaskivnyuk <m.vaskivnyuk@gmail.com>
2020-08-26 11:53:32 +0300

    first commit

---
+++ src/Components/Keyboard.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Tabs() {
+    return (
+        <div>
+            <div>Symbol</div>
+            <div>BACKSPACE</div>
+            <div>CLEAR</div>
+        </div>
+    );
+}
+
+export default Tabs;
---
+++ src/Components/Main.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Weights from './Weights';
+import Tabs from './Tabs';
+import Tara from './Tara';
+import Print from './Print';
+import Close from './Close';
+import Keyboard from './Keyboard';
+
+function Main() {
+    return (
+        <div>
+            <Weights />
+            <Tabs />
+            <Tara />
+            <Print />
+            <Close />
+            <Keyboard />
+        </div>
+    );
+}
+
+export default Main;
