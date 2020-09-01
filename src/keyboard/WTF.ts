98d5b67dc88619c9fc1519d1a1de1a2b496a7dfd
NeverSmileMan <kamukag3eserg@gmail.com>
2020-09-01 19:52:18 +0300

    Update WTF.ts

--- src/keyboard/WTF.ts
+++ src/keyboard/WTF.ts
@@ -2,145 +2,143 @@ export const WTF =  {
   grid: {
     display: 'grid',
     gridGap: '0.4em',
+    gridTemplateColumns: 'repeat(1, 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr)',
   },
 	0: {
-    gridColumn: 1 / 3,
+    gridColumn: '1 / 4',
     gridRow: 1,
 	},
 	1: {
-    gridColumn: 4 / 6,
+    gridColumn: '4 / 7',
     gridRow: 1,
 	},
 	2: {
-    gridColumn: 7 / 9,
+    gridColumn: '7 / 10',
     gridRow: 1,
 	},
 	3: {
-    gridColumn: 10 / 12,
+    gridColumn: '10 / 13',
     gridRow: 1,
 	},
 	4: {
-    gridColumn: 13 / 15,
+    gridColumn: '13 / 16',
     gridRow: 1,
 	},
 	5: {
-    gridColumn: 16 / 18,
+    gridColumn: '16 / 19',
     gridRow: 1,
 	},
 	6: {
-    gridColumn: 19 / 21,
+    gridColumn: '19 / 22',
     gridRow: 1,
 	},
 	7: {
-    gridColumn: 22 / 24,
+    gridColumn: '22 / 25',
     gridRow: 1,
 	},
 	8: {
-    gridColumn: 25 / 27,
+    gridColumn: '25 / 28',
     gridRow: 1,
 	},
 	9: {
-    gridColumn: 28 / 30,
+    gridColumn: '28 / 31',
     gridRow: 1,
 	},
 	10: {
-    gridColumn: 31 / 33,
+    gridColumn: '31 / 34',
     gridRow: 1,
 	},
 	11: {
-    gridColumn: 34 / 36,
+    gridColumn: '34 / 37',
     gridRow: 1,
 	},
 	12: {
-    gridColumn: 2 / 4,
+    gridColumn: '2 / 5',
     gridRow: 2,
 	},
 	13: {
-    gridColumn: 4 / 6,
+    gridColumn: '5 / 8',
     gridRow: 2,
 	},
 	14: {
-    gridColumn: 7 / 9,
+    gridColumn: '8 / 11',
     gridRow: 2,
 	},
 	15: {
-    gridColumn: 10 / 12,
+    gridColumn: '11 / 14',
     gridRow: 2,
 	},
 	16: {
-    gridColumn: 13 / 15,
+    gridColumn: '14 / 17',
     gridRow: 2,
 	},
 	17: {
-    gridColumn: 16 / 18,
+    gridColumn: '17 / 20',
     gridRow: 2,
 	},
 	18: {
-    gridColumn: 19 / 21,
+    gridColumn: '20 / 23',
     gridRow: 2,
 	},
 	19: {
-    gridColumn: 22 / 24,
+    gridColumn: '23 / 26',
     gridRow: 2,
 	},
 	20: {
-    gridColumn: 25 / 27,
+    gridColumn: '26 / 29',
     gridRow: 2,
 	},
 	21: {
-    gridColumn: 28 / 30,
+    gridColumn: '29 / 32',
     gridRow: 2,
 	},
 	22: {
-    gridColumn: 31 / 33,
+    gridColumn: '32 / 35',
     gridRow: 2,
 	},
 	23: {
-    gridColumn: 35 / 37,
+    gridColumn: '35 / 38',
     gridRow: 2,
 	},
 	24: {
-    gridColumn: 1 / 3,
+    gridColumn: '1 / 4',
     gridRow: 3,
 	},
 	25: {
-    gridColumn: 4 / 6,
+    gridColumn: '4 / 7',
     gridRow: 3,
 	},
 	26: {
-    gridColumn: 7 / 9,
+    gridColumn: '7 / 10',
     gridRow: 3,
 	},
 	27: {
-    gridColumn: 10 / 12,
+    gridColumn: '10 / 13',
     gridRow: 3,
 	},
 	28: {
-    gridColumn: 13 / 15,
+    gridColumn: '13 / 16',
     gridRow: 3,
 	},
 	29: {
-    gridColumn: 16 / 18,
+    gridColumn: '16 / 19',
     gridRow: 3,
 	},
 	30: {
-    gridColumn: 19 / 21,
+    gridColumn: '19 / 22',
     gridRow: 3,
 	},
 	31: {
-    gridColumn: 22 / 24,
+    gridColumn: '22 / 25',
     gridRow: 3,
 	},
 	32: {
-    gridColumn: 25 / 27,
+    gridColumn: '25 / 28',
     gridRow: 3,
 	},
 	33: {
-    gridColumn: 28 / 30,
+    gridColumn: '28 / 37',
     gridRow: 3,
 	},
-	34: {
-    gridColumn: 31 / 33,
-    gridRow: 3,
-	},
+
 }
