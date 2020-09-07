99c222f3e488dc8be4d5de17ef980ca84a532abd
Oleg Tryguba <trigyba.oleg@gmail.com>
2020-09-07 14:08:02 +0300

    added folders

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import {makeStyles} from "@material-ui/styles";
-import Tabs from "./Tabs";
-import Hints from "./Hints";
-import HomeButton from "./HomeButton";
+import Tabs from "./header/Tabs";
+import Hints from "./header/Hints";
+import HomeButton from "./header/HomeButton";
 
 
 function App() {
