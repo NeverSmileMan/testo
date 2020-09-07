ef51b6644a873364201565455d7a00da4289e81f
mykhailo-vaskivnyuk <m.vaskivnyuk@gmail.com>
2020-09-07 17:47:11 +0300

    iput + order-info + list

--- src/Components/List.tsx
+++ src/Components/List.tsx
@@ -1,5 +1,41 @@
 import React, { useState } from 'react';
 import InputObject from '../data.structure/Input';
+import { makeStyles } from '@material-ui/styles';
+
+const useStyles = makeStyles({
+    'list': {
+        backgroundColor: 'rgb(0, 153, 255)',
+        '& ul': {
+            listStyle: 'none',
+            margin: '0px',
+            padding: '0px',
+            overflowY: 'auto',
+            textAlign: 'left',
+            maxHeight: '368px',
+        },
+        '& li': {
+            fontSize: '0.8em',
+            borderBottom: 'solid 1px black',
+            paddingLeft: '10px',
+            paddingRight: '10px',
+            backgroundColor: 'white',
+        },
+        '& span:first-child': {
+            display: 'inline-block',
+            width: '50px',
+        },
+        '& .not-found': {
+            width: '100%',
+            height: '300px',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            color: 'white',
+            fontWeight: 'bold',
+        },
+
+    },
+});
 
 const list = InputObject.getInputListInstance().getListInstance();
 
@@ -11,6 +47,7 @@ const onClick = (event: React.MouseEvent<HTMLDivElement>) => {
 }
 
 function List() {
+    const classes = useStyles();
     const [, setState] = useState({});
 
     useState(() => {
@@ -21,13 +58,26 @@ function List() {
 
     const itemsArray = list.getItems();
 
+    console.log(itemsArray);
     if (!itemsArray) return null;
     
-    const items = itemsArray.map((item, i) => <div key={i} data-item-index={i}>{item.name}</div>);
+    const items = itemsArray.map((item, i) => 
+        <li key={i} data-item-index={i}>
+            <span>{item.plu}</span>
+            <span>{item.name}</span>
+        </li>
+    );
 
     return (
-        <div className='list' onClick={onClick}>
-            {items.length ? items : 'НІЧОГО НЕ ЗНАЙДЕНО'}
+        <div className={`${classes.list} list`} onClick={onClick}>
+            <ul>
+                {items.length ?
+                    items :
+                    <div className='not-found'>
+                        <div>НІЧОГО НЕ ЗНАЙДЕНО</div>
+                    </div>
+                }
+            </ul>
         </div>
     );
 }
