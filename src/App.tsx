8aa1d7312fee5141aa4e6147a0787a492a221312
NeverSmileMan <kamukag3eserg@gmail.com>
2020-09-07 21:56:31 +0300

    clear

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,69 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useTheme, makeStyles  } from '@material-ui/core/styles';
+
+
+const useStyles = makeStyles({
+	scale: {
+    width: '100%',
+    height: '15.8854%',
+  },
+	mainWrap: {
+    width: '100%',
+    height: '84.1146%',
+  },
+	main: {
+    width: '100%',
+    height: '70%',
+  },
+	keyboardWrap: {
+    width: '100%',
+    height: '30%',
+  },
+    '@global': {
+      '::-webkit-scrollbar': {
+        width: '40px',
+      },
+      '::-webkit-scrollbar-track': {
+        boxShadow: 'inset 0 0 5px #e4e4e4',
+      },
+      '::-webkit-scrollbar-thumb': {
+        background: '#fff',
+        borderRadius: '10px',
+      },
+    },
+    '#root': {
+      width: '100%',
+      height: '100%',
+      margin: 0,
+      padding: 0,
+    }
+  });
+
 
 function App() {
+
+
+	const classes = useStyles();
+
+  
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+        <div className={classes.scale}>
+        </div>
+        <div className={classes.mainWrap}>
+          <div className={classes.main}>
+
+          </div>
+          <div className={classes.keyboardWrap}>
+
+          </div>
+        </div>
+     </>
   );
 }
+
+
 
 export default App;
