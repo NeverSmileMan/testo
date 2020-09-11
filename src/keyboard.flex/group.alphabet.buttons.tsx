62b1b66f18f02b96e3b751a93ffb9a915c288d93
NeverSmileMan <kamukag3eserg@gmail.com>
2020-09-11 09:48:01 +0300

    Update group.alphabet.buttons.tsx

--- src/keyboard.flex/group.alphabet.buttons.tsx
+++ src/keyboard.flex/group.alphabet.buttons.tsx
@@ -37,12 +37,12 @@ export default function GroupAlphabetButtons({ opts, service }: any) {
 		<div className={classes.keyboardAlphabet}>
 			{opts.keys[layout.name].map((item: Key[], i: number) => {
 				return (
-					<>
+					<React.Fragment key={`${i}fr`}>
 						<div key={`${i}x`} className={classes.row}>
 							{i % 2 ? <div key={`${i}i`} className={classes.offset}></div> : null}
-							{item.map((item: Key, index) => (
+							{item.map((item: Key, index, arr) => (
 								<Button
-									key={index}
+									key={`${index}`}
 									value={item}
 									callback={service[opts.action]}
 									className={item === ' ' ? classes.space : ''}
@@ -50,8 +50,8 @@ export default function GroupAlphabetButtons({ opts, service }: any) {
 							))}
 							{!(i % 2) ? <div key={`${i}o`} className={classes.offset}></div> : null}
 						</div>
-						{i < opts.keys[layout.name].length - 1 ? <div key={`${i}z`} className={classes.spacer}></div> : null}
-					</>
+							{i < opts.keys[layout.name].length - 1 ? <div key={`${i}z`} className={classes.spacer}></div> : null}
+					</React.Fragment>
 				);
 			})}
 		</div>
