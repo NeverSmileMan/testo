793f5c32c02416fc83a492556415476bdad466ba
mykhailo-vaskivnyuk <m.vaskivnyuk@gmail.com>
2020-09-15 02:18:25 +0300

    del modal service

--- src/data.structure/Keyboard.ts
+++ src/data.structure/Keyboard.ts
@@ -1,32 +1,11 @@
 import { IInput } from './Input';
 
 export interface IKeyboard {
-    // addSet: (setName: string, set: string[]) => void;
-    // delSet: (setName: string) => void;
-    // getSet: (setName: string) => string[] | undefined;
-    // getSymbol: (setName: string, index: number) => string | undefined;
     setActiveInput: (input: IInput | null) => void;
 }
 
 class Keyboard implements IKeyboard {
     private _input?: IInput | null;
-    // private _sets: Map<string, string[]> = new Map();
-
-    // addSet(setName: string, set: string[]) {
-    //     this._sets.set(setName, set);
-    // }
-
-    // delSet(setName: string) {
-    //     this._sets.delete(setName);
-    // }
-
-    // getSet(setName: string) {
-    //     return this._sets.get(setName);
-    // }
-
-    // getSymbol(setName: string, index: number) {
-    //     return this._sets.get(setName)?.[index];
-    // }
 
     onClick(key: string) {
         this._input?.pressKey(key);
