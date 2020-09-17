63848dc7d824d2f0427ca7d105e7e50dafa555fe
NeverSmileMan <kamukag3eserg@gmail.com>
2020-09-17 19:02:37 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ import Search from './searchPanel/search/Search';
 import OrderInfo from './searchPanel/orderInfo/OrderInfo';
 import ModalWindow from './functional-buttons/modal.wind/modal.wind';
 import { useTabs } from './tabs/use.Tab.hook';
-import { IItem } from './searchPanel/search/itemsData'
+import { IItem } from './searchPanel/search/itemsData';
 import { Hints } from './custom/variables';
 import { useHints } from './custom/hooks';
 //---------plugs---------------
@@ -18,161 +18,166 @@ import { ScalePlug } from './plugs/scale';
 //-----------------------------
 
 const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    header: {
-      width: '100%',
-      height: '14%',
-      display: 'flex',
-    },
-    tab: {
-      width: '100%',
-      height: '100%',
-    },
-    info: {
-      width: '100%',
-      height: '100%',
-      display: 'flex',
-      justifyContent: 'space-around',
-    },
-    bodyWrap: {
-      width: '100%',
-      height: '86%',
-      display: 'flex',
-    },
-    searchPanel: {
-      width: '100%',
-      height: '17%',
-      display: 'flex',
-    },
-    body: {
-      width: '92%',
-      height: '100%',
-      borderRadius: '0 0 0 .4em',
-      borderBottom: `2px ${theme.palette.primary.main} solid`,
-      borderLeft: `2px ${theme.palette.primary.main} solid`,
-      boxSizing: 'border-box',
-      overflow: 'hidden',
-      position: 'relative',
-    },
-    sideButtons: {
-      width: '8%',
-      height: '100%',
-      display: 'flex',
-    },
-  }),
+	createStyles({
+		header: {
+			width: '100%',
+			height: '14%',
+			display: 'flex',
+		},
+		tab: {
+			width: '100%',
+			height: '100%',
+		},
+		info: {
+			width: '100%',
+			height: '100%',
+			display: 'flex',
+			justifyContent: 'space-around',
+		},
+		bodyWrap: {
+			width: '100%',
+			height: '86%',
+			display: 'flex',
+		},
+		searchPanel: {
+			width: '100%',
+			height: '17%',
+			display: 'flex',
+		},
+		body: {
+			width: '92%',
+			height: '100%',
+			borderRadius: '0 0 0 .4em',
+			borderBottom: `2px ${theme.palette.primary.main} solid`,
+			borderLeft: `2px ${theme.palette.primary.main} solid`,
+			boxSizing: 'border-box',
+			overflow: 'hidden',
+			position: 'relative',
+		},
+		sideButtons: {
+			width: '8%',
+			height: '100%',
+			display: 'flex',
+		},
+	}),
 );
 
 export const MainContext = createContext({
-  modalType: '' as string | null,
-  setType: (val: string | null): any => { },
-  deleteTab: () => { },
-  addTab: (e: React.MouseEvent<HTMLDivElement>) => { },
-  confirmClose: () => { },
-  setActiveTab: (val: any) => { },
-  addItem: (item: any) => true as boolean,
-  activeTab: '' as any,
-  showPrint: () => { },
-  changeHint: (str: Hints, likeError?: boolean) => {},
-  submitValueCalc: (num: number) => { },
-  setSelectedItem: (() => { }) as React.Dispatch<React.SetStateAction<IItem>>,
-  setCalcValue: (() => { }) as React.Dispatch<React.SetStateAction<number>>,
-  calcValue: 0 as number,
-  activeTara: 0 as number,
-  selectedItem: {} as IItem,
+	modalType: '' as string | null,
+	setType: (val: string | null): any => {},
+	deleteTab: () => {},
+	addTab: (e: React.MouseEvent<HTMLDivElement>) => {},
+	confirmClose: () => {},
+	setActiveTab: (val: any) => {},
+	addItem: (item: any) => true as boolean,
+	activeTab: '' as any,
+	showPrint: () => {},
+	changeHint: (str: Hints, likeError?: boolean) => {},
+	submitValueCalc: (num: number) => {},
+	setSelectedItem: (() => {}) as React.Dispatch<React.SetStateAction<IItem>>,
+	setCalcValue: (() => {}) as React.Dispatch<React.SetStateAction<number>>,
+	calcValue: 0 as number,
+	activeTara: 0 as number,
+	selectedItem: {} as IItem,
 });
 
 export default function Main() {
   const { tab, sideButtons, info, bodyWrap, body, header, searchPanel } = useStyles();
-  const [modalType, setModalType] = useState(null as string | null);
-  const [selectedItem, setSelectedItem] = useState({} as IItem)
-  const setType = (type: string | null): any => () => setModalType(type);
-  const [calcValue, setCalcValue] = useState(0);
-  const submitValueCalc = (val: number) => {
-    setCalcValue(val);
-    setType(null)();
-  };
-
   const [hint, error, changeHint] = useHints();
-
-  const [
-    tabItems,
-    activeTab,
-    activeItem,
-    setActiveTab,
-    setActiveItem,
-    addItem,
-    deleteItem,
-    addTab,
-    deleteTab,
-    setTara,
-    print
-  ] = useTabs(changeHint, ScalePlug, calcValue);
-
-  const confirmClose = useCallback(() => {
-    setType(null)();
-    deleteTab();
-  }, [setType, deleteTab]);
+  
 
 
 
-  const showPrint = () => {
-    setType(null)();
-    print()
-  }
 
-  const showTara = () => {
-    setTara()
-    changeHint(Hints.PositiveWeight)
-  }
+  
+  
+	const [modalType, setModalType] = useState(null as string | null);
+	const [selectedItem, setSelectedItem] = useState({} as IItem);
+	const setType = (type: string | null): any => () => setModalType(type);
+	const [calcValue, setCalcValue] = useState(0);
+	const submitValueCalc = (val: number) => {
+    setCalcValue(val);
+		setType(null)();
+	};
+  
+    const [
+      tabItems,
+      activeTab,
+      activeItem,
+      setActiveTab,
+      setActiveItem,
+      addItem,
+      deleteItem,
+      addTab,
+      deleteTab,
+      setTara,
+      print,
+    ] = useTabs(changeHint, ScalePlug, calcValue);
 
-  useEffect(() => {
-    showTara()
-  }, [calcValue])
-  const activeTara=tabItems[activeTab].tara;
 
-  return (
-    <MainContext.Provider
-      value={{
-        modalType,
-        setType,
-        deleteTab,
-        addTab,
-        confirmClose,
-        setActiveTab,
-        addItem,
-        activeTab,
-        showPrint,
-        changeHint,
-        submitValueCalc,
-        setSelectedItem,
-        setCalcValue,
-        calcValue,
-        activeTara:tabItems[activeTab].tara,
-        selectedItem
-      }}
-    >
-      <div className={header}>
-        <div className={tab}>
-          <Tabs tabs={tabItems} />
-        </div>
-        <div className={info}>
-          <Hint hint={hint} error={error} />
-          <HomeButton />
-        </div>
-      </div>
-      <div className={bodyWrap}>
-        <div className={body}>
-          <div className={searchPanel}>
-            <Search />
-            <OrderInfo value={tabItems[activeTab].items} activeItem={activeItem} onClick={deleteItem} />
-          </div>
-          <AddedItemsTable values={tabItems[activeTab].items} onClick={setActiveItem} active={activeItem} />
-        </div>
-        <div className={sideButtons}>
-          <GroupBtn />
-        </div>
-        {modalType && <ModalWindow />}
-      </div>
-    </MainContext.Provider>
-  );
+	const confirmClose = useCallback(() => {
+		setType(null)();
+		deleteTab();
+	}, [setType, deleteTab]);
+
+	const showPrint = () => {
+		setType(null)();
+		print();
+	};
+
+	// const showTara = () => {
+	// 	setTara();
+	// 	changeHint(Hints.PositiveWeight);
+	// };
+
+	// useEffect(() => {
+	// 	showTara();
+	// }, [calcValue]);
+	// const activeTara = tabItems[activeTab].tara;
+
+	return (
+		<MainContext.Provider
+			value={{
+				modalType,
+				setType,
+				deleteTab,
+				addTab,
+				confirmClose,
+				setActiveTab,
+				addItem,
+				activeTab,
+				showPrint,
+				changeHint,
+				submitValueCalc,
+				setSelectedItem,
+				setCalcValue,
+				calcValue,
+				activeTara: tabItems[activeTab].tara,
+				selectedItem,
+			}}
+		>
+			<div className={header}>
+				<div className={tab}>
+					<Tabs tabs={tabItems} />
+				</div>
+				<div className={info}>
+					<Hint hint={hint} error={error} />
+					<HomeButton />
+				</div>
+			</div>
+			<div className={bodyWrap}>
+				<div className={body}>
+					<div className={searchPanel}>
+						<Search />
+						<OrderInfo value={tabItems[activeTab].items} activeItem={activeItem} onClick={deleteItem} />
+					</div>
+					<AddedItemsTable values={tabItems[activeTab].items} onClick={setActiveItem} active={activeItem} />
+				</div>
+				<div className={sideButtons}>
+					<GroupBtn />
+				</div>
+				{modalType && <ModalWindow />}
+			</div>
+		</MainContext.Provider>
+	);
 }
