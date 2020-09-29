d49bdcede16ed46e6b43b0acc340e47179ed4df1
NeverSmileMan <kamukag3eserg@gmail.com>
2020-09-29 15:03:04 +0300

    1

--- src/components/keyboard/button/button.tsx
+++ src/components/keyboard/button/button.tsx
@@ -1,14 +1,13 @@
 import React, { useCallback, FC } from 'react';
-import { Key, Actions } from '../keyboard.main/keyboard.interfaces';
 import { useStylesButton } from './button.styles';
 
-interface Props {
-  callback: Actions;
-  value: Key;
-  className: string;
+interface Props<T> {
+  callback: (value: T) => void;
+  value: T;
+  className: T;
 }
 
-export const Button: FC<Props> = ({ callback, value, children = null, className }) => {
+export const Button: FC<Props<string>> = ({ callback, value, children = null, className }) => {
   const classes = useStylesButton();
 
   const onClick = useCallback(() => {
--- src/components/keyboard/keyboard.numeric/keyboard.numeric.tsx
+++ src/components/keyboard/keyboard.numeric/keyboard.numeric.tsx
@@ -1,36 +1,38 @@
-import React, { ReactElement } from 'react';
+import React, { FC } from 'react';
 import { Button } from '../button/button';
-import { Numeric, Key, Actions } from '../keyboard.main/keyboard.interfaces';
+import { Numeric, Key } from '../keyboard.main/keyboard.interfaces';
 
-interface Props {
-  options: Numeric;
-  onClick: Actions;
-  classes: any;
+interface Props<T> {
+	options: Numeric;
+	onClick: (value: T) => void;
+	classes: Record<any, string>;
 }
+
 type BtnClass =
-  | 'nkey_0'
-  | 'nkey_1'
-  | 'nkey_2'
-  | 'nkey_3'
-  | 'nkey_4'
-  | 'nkey_5'
-  | 'nkey_6'
-  | 'nkey_7'
-  | 'nkey_8'
-  | 'nkey_9';
+	| 'nkey_0'
+	| 'nkey_1'
+	| 'nkey_2'
+	| 'nkey_3'
+	| 'nkey_4'
+	| 'nkey_5'
+	| 'nkey_6'
+	| 'nkey_7'
+	| 'nkey_8'
+	| 'nkey_9';
 
-export function KeyboardNumeric({ options, onClick, classes }: Props): ReactElement {
+export const KeyboardNumeric: FC<Props<string>> = (props) => {
+  const { options, onClick, classes } = props;
   
-  return (
-    <div className={classes.keyboardNumeric}>
-      {options.keys.map((item: Key, id: number) => (
-        <Button
-          key={item}
-          value={item}
-          callback={onClick}
-          className={`${classes[`nkey_${id}` as BtnClass]} ${classes.btnNumeric}`}
-        />
-      ))}
-    </div>
-  );
-}
+	return (
+		<div className={classes.keyboardNumeric}>
+			{options.keys.map((item: Key, id: number) => (
+				<Button
+					key={item}
+					value={item}
+					callback={onClick}
+					className={`${classes[`nkey_${id}` as BtnClass]} ${classes.btn}`}
+				/>
+			))}
+		</div>
+	);
+};
