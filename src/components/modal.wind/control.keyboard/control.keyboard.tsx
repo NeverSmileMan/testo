77f0cd0b3a3a427d224d8b380ea63139eac74d1f
m.asaulenko <mashkajazz@gmail.com>
2020-10-01 23:19:56 +0300

    add missing changes

--- src/components/modal.wind/control.keyboard/control.keyboard.tsx
+++ src/components/modal.wind/control.keyboard/control.keyboard.tsx
@@ -1,8 +1,10 @@
 import React, { useCallback } from 'react';
-import {KeyboardBtn} from '../keyboard.btn/keyboard.btn';
+// import {KeyboardBtn} from '../keyboard.btn/keyboard.btn';
+import { Button } from '../../keyboard/button/button';
 import { useTheme } from '@material-ui/styles';
 import { Theme } from '@material-ui/core/styles/createMuiTheme';
 import { useStyle } from './control.keyboard.style';
+import { styles } from '../../homeButton/HomeButton.styles';
 
 interface Prop {
   onClick: {
@@ -11,32 +13,48 @@ interface Prop {
   },
   inputValue: number
 }
+interface Styles {
+  textColor?: string,
+  colorBtn?: string,
+  fontSize?: string,
+  filter?: string
+}
 
 export const ControlKeyboard = ({ onClick, inputValue }: Prop) => {
   const theme = useTheme<Theme>();
   const control = [<span>&#8592;</span>, <span>&#10003;</span>];
   const { controlContainer } = useStyle();
-  const controlBtnClass = useCallback((index): string =>
-    (index === 1 && inputValue <= 0)
-      ? 'backLigth'
-      : '',
-    [inputValue])
 
+
+  const styles = {
+    textColor: '#fff',
+    colorBtn: theme.palette.primary.main,
+    fontSize: '0.8em'
+  }
+
+  const controlBtnClass = useCallback((index:number): Styles =>
+    (index === 1 && inputValue <= 0)
+      ? styles
+      : { ...styles, filter: 'brightness(150%)' },
+    [inputValue, styles])
   return (
     <div className={controlContainer}>
-      {control.map((val, index) => (
-        <KeyboardBtn
-          onClick={index === control.length - 1 ? () => onClick.submit() : () => onClick.delete()}
-          key={index}
-          // btnName={val}
-          textColor='#fff'
-          colorBtn={theme.palette.primary.main}
-          nameClass={controlBtnClass(index)} 
-        >
-          {val}
-        </KeyboardBtn>
+      {control.map((val, index, array) => {
+        const getControlRenderOnClick = (arr: Array<JSX.Element>, ind: number) => {
+          ind === arr.length - 1 ? onClick.submit() : onClick.delete()
+        }
+        return (
+          <Button
+            onClick={() => getControlRenderOnClick(array, index)}
+            key={index}
+            styles={controlBtnClass(index)}
+            className=''
+          >
+            { val}
+          </Button>
         )
+      }
       )}
-    </div>
+    </div >
   )
 }
