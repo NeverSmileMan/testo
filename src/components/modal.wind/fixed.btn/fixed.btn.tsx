79ef380c290267d85f8fe6c1f1bf6555349150a5
m.asaulenko <mashkajazz@gmail.com>
2020-10-01 15:59:23 +0300

    add keyboard btn

--- src/components/modal.wind/fixed.btn/fixed.btn.tsx
+++ src/components/modal.wind/fixed.btn/fixed.btn.tsx
@@ -1,9 +1,10 @@
 import React, { useCallback } from 'react';
-import {Button} from '../../keyboard/button/button' 
+import { Button } from '../../keyboard/button/button'
 // import { KeyboardBtn } from '../keyboard.btn/keyboard.btn';
 import { useTheme } from '@material-ui/styles';
 import { Theme } from '@material-ui/core/styles/createMuiTheme';
 import { useStyle } from './fixed.btn.style';
+import { styles } from '../../homeButton/HomeButton.styles';
 
 interface Prop {
   strNumber: Array<number>;
@@ -16,27 +17,28 @@ export const FixedBtn = ({ strNumber, units, modalClose, submitValueCalc }: Prop
   const cls = useStyle();
   const theme = useTheme<Theme>();
 
-  const submitWeight = (val: number):void => {
+  const submitWeight = (val: number): void => {
     submitValueCalc(val);
     modalClose();
   };
+  const styles = {
+    fontSize:'0.8em',
+    textTransform:'none',
+    border:theme.palette.primary.main,
+  }
 
   return (
     <div className={cls.btnContainer}>
       {strNumber.map((val) => (
         <Button
-          callback={submitWeight}
-          value={val}
+          onClick={() => submitWeight(val)}
           key={val}
           className={''}
-          fontSize={'0.8em'}
-          textTransform='none'
-          // btnName={<>{val} {units}</>}
-          border={theme.palette.primary.main}
+          styles={styles}
         >
           {`${val} ${units}`}
         </Button>
-        ))}
+      ))}
     </div>
   )
 }
--- src/components/modal.wind/modal.tara/input.tara/input.weight.tsx
+++ src/components/modal.wind/modal.tara/input.tara/input.weight.tsx
@@ -1,10 +1,12 @@
 import React, { useState, useCallback } from 'react';
-import {NumberKeyboard} from '../../number.keyboard/numder.keyboard';
 import {ControlKeyboard} from '../../control.keyboard/control.keyboard';
 import {HeadInput} from '../../head.input/head.input';
 import {useHints} from '../../../hint/hint.provider';
 import {useStyle} from './input.weight.style';
-
+import {KeyboardNumeric} from '../../../keyboard/keyboard.numeric/keyboard.numeric';
+import {keyboardSettings} from '../../../keyboard/keyboard.main/keyboard.settings';
+import {useStyles} from '../../../keyboard/keyboard.numeric/keyboard.numeric.styles';
+// import {useStyleNum} from '../../number.keyboard/number.keyboard.style';
 interface Prop {
   submitValueCalc: (num:number) => void;
   modalClose: ()=>void;
@@ -14,9 +16,11 @@ export const InputWeight = ({modalClose, submitValueCalc}:Prop) => {
   const { inputContainer, keyboardContainer } = useStyle();
   const { changeHint, Hints } = useHints();
   const [inputValue, setWeihghtTara] = useState(0)
+  const classesNumeric = useStyles({gridColumn_9 : '1 / 3'});
 
-  const getWeight = useCallback((num: number): any => () => {
-    const weitght = inputValue * 10 + num;
+  const getWeight = useCallback((num: string): any => {
+    console.log ('well come', num);
+    const weitght = inputValue * 10 + Number(num);
     return weitght >= 5000 ? changeHint(Hints.MaxWeight, true) : setWeihghtTara(weitght)
   }, [setWeihghtTara, inputValue, changeHint])
 
@@ -41,7 +45,7 @@ export const InputWeight = ({modalClose, submitValueCalc}:Prop) => {
         inputValue={(inputValue / 1000).toFixed(3)}
         inputName={NameCalc} />
       <div className={keyboardContainer}>
-        <NumberKeyboard onClick={getWeight} />
+        <KeyboardNumeric options={keyboardSettings.numeric} onClick={getWeight} styles={classesNumeric} />
         <ControlKeyboard inputValue={inputValue} onClick={controlOnckick} />
       </div>
     </div>
