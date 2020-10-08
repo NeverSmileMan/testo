0c6b06f794f1173e58f47ee92625f571eb636e90
NeverSmileMan <kamukag3eserg@gmail.com>
2020-10-08 09:47:36 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  div: {
+    // width: '20px',
+    // height: '20px',
+    width: (props: any) => props.width ?? '20px',
+    height: (props: any) => props.height ?? '20px',
+  },
+});
+
+const divs = Array(10).fill(1);
 
 function App() {
+  const [state, setState] = React.useState(true);
+  const onClick = React.useCallback(() => {
+    setState(!state);
+  }, [state]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Button state={state} onClick={onClick} />
+      {state ? divs.map((item) => <Div item={item} />) : null}
     </div>
   );
 }
+
+const Button = ({ state, onClick }: any) => {
+  return <button onClick={onClick}>{state ? 'close' : 'show'}</button>;
+};
+
+const Div = ({ item }: any) => {
+  const classes = useStyles();
+  return <div className={classes.div}>{item}</div>;
+};
 
 export default App;
